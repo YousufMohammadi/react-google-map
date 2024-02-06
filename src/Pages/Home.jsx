5a96971bf06657c79e40afc037630c4a377eead2
YousufMohammadi <yousufmohammadi3222@gmail.com>
2024-02-06 10:19:42 +0000

    google map api completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-function Home() {
+const Home = () => {
+  const mapStyles = {
+    height: '100vh',
+    width: '100%',
+  };
+
+  const defaultCenter = {
+    lat: 40.748817,
+    lng: -73.985428,
+  };
+
   return (
-    <div>Home</div>
-  )
-}
+    <LoadScript googleMapsApiKey="YOUR_API_KEY">
+      <GoogleMap
+        mapContainerStyle={mapStyles}
+        zoom={13}
+        center={defaultCenter}
+      >
+        {/* Add markers or other components here */}
+        <Marker position={defaultCenter} />
+      </GoogleMap>
+    </LoadScript>
+  );
+};
 
-export default Home
+export default Home;
